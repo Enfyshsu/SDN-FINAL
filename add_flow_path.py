2db8f7f3a4470491276fefef589d82e226fca317
ensyshsu <wch8831@gmail.com>
2022-01-07 15:13:22 +0800

    main and back path

--- add_flow_path.py
+++ add_flow_path.py
@@ -8,32 +8,39 @@ def add_flow_path(filename, path):
     with open(filename, 'w') as f:    
         json.dump(data, f, indent=4)
 
+def inputPath(n, path, kind):
+    path[kind] = []
+    for i in range(n):
+        device_id, output_port = input("<device_id> <output_port>: ").split()
+        node = dict({
+                "device_id": str(device_id), 
+                "output_port": str(output_port)
+        })
+        
+        path[kind].append(node)
 
 def main():
-    filename = "flow_path.json"
-    n = int(input("Enter device number: "))
+    filename = "flow_path_2.json"
     path = {}
-    print("Enter main path: ")
-    path['main'] = []
-    for i in range(n):
-        device_id, output_port = input("<device_id output_port>: ").split()
-        node = dict({
-                "device_id": str(device_id), 
-                "output_port": str(output_port)
-        })
-        
-        path['main'].append(node)
+    src_host = input("Enter source host: ")
+    dst_host = input("Enter destination host: ")
+    path['src_ip'] = "10.0.0." + src_host
+    path['dst_ip'] = "10.0.0." + dst_host
 
-    print("Enter backup path: ")
-    path['backup'] = []
-    for i in range(n):
-        device_id, output_port = input("<device_id output_port>: ").split()
-        node = dict({
-                "device_id": str(device_id), 
-                "output_port": str(output_port)
-        })
-        
-        path['backup'].append(node)
+    n = int(input("Enter main path device number: "))
+    print("Enter main path: ")
+    inputPath(n, path, 'main_A')
+
+    print("Enter main_B path: ")
+    inputPath(n, path, 'main_B')
+
+    n = int(input("Enter backup path device number: "))
+    print("Enter backup_A path: ")
+    inputPath(n, path, 'backup_A')
+
+    print("Enter backup_B path: ")
+    inputPath(n, path, 'backup_B')
+    
     #print(path)   
     add_flow_path(filename, path)
 
